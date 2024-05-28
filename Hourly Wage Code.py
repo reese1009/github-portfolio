9e4056acd3b84208492db00f6236306ceccc9878
Reese Norwood <116681286+reese1009@users.noreply.github.com>
2024-05-28 16:06:41 -0400

    Update and rename main.py to Hourly Wage Code.py

---
+++ Hourly Wage Code.py
@@ -0,0 +1,5 @@
+hourly_wage=int(input('Enter hourly wage:'))
+hours_per_week = 8
+
+print('Weekly salary is', hourly_wage * hours_per_week)
+print(f'Weekly salary is {hourly_wage * hours_per_week}')
